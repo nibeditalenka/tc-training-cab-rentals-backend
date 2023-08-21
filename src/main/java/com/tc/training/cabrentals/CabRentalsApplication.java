fbedfe56550a0e29174a457565ff5affe8ba14f1
Nibedita <nibedita.lenka@thoughtclan.com>
2023-08-21 14:45:56 +0530

    created project

---
+++ src/main/java/com/tc/training/cabrentals/CabRentalsApplication.java
@@ -0,0 +1,13 @@
+package com.tc.training.cabrentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabRentalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CabRentalsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tc/training/cabrentals/CabRentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tc.training.cabrentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabRentalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
