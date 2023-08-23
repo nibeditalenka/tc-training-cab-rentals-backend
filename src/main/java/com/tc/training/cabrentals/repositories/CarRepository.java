07469a352d108ac327e611f5be86b705fc3e2fc2
Chetan <chetan.shukla@thoughtclan.com>
2023-08-23 17:33:54 +0530

    added q class

--- src/main/java/com/tc/training/cabrentals/repositories/CarRepository.java
+++ src/main/java/com/tc/training/cabrentals/repositories/CarRepository.java
@@ -3,12 +3,11 @@ package com.tc.training.cabrentals.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.tc.training.cabrentals.entities.Car;
 
 @Repository
-public interface CarRepository extends JpaRepository<Car, UUID>, QuerydslPredicateExecutor<Car> {
+public interface CarRepository extends JpaRepository<Car, UUID> {
 
 }
