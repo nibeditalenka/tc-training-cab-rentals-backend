40301f5173021fe7bc6502429244dbb7a22ace57
Nibedita <nibedita.lenka@thoughtclan.com>
2023-08-24 16:36:07 +0530

    added swagger, and updated jar

--- src/main/java/com/tc/training/cabrentals/CabRentalsApplication.java
+++ src/main/java/com/tc/training/cabrentals/CabRentalsApplication.java
@@ -4,8 +4,16 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@OpenAPIDefinition( info = @Info( title = "cab-rentals", version = "v3" ) )
+@CrossOrigin
 public class CabRentalsApplication {
 
   public static void main( String[] args ) {
@@ -15,6 +23,16 @@ public class CabRentalsApplication {
   @Bean
   public ModelMapper modelMapper() {
     return new ModelMapper();
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings( CorsRegistry registry ) {
+        registry.addMapping( "/**" ).allowedOrigins( "*" ).allowedHeaders( "*" ).allowedMethods( "*" );
+      }
+    };
   }
 
 }
